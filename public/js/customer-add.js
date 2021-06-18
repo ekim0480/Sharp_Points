0a0f3f1f66664292f8363ac1f41a30ca1b24bd10
EK <ekim0480@gmail.com>
2021-06-18 14:51:12 -0500

    bugfix, gender.val() wasnt working

--- public/js/customer-add.js
+++ public/js/customer-add.js
@@ -38,7 +38,7 @@ $(document).ready(function () {
       var newCustomer = {
         firstName: firstNameInput.val().trim(),
         lastName: lastNameInput.val().trim(),
-        gender: genderInput.val().trim(),
+        gender: genderInput,
         dob: dobInput.val().trim(),
         phone: phoneInput.val().trim(),
         email: emailInput.val().trim(),
