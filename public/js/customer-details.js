e9e678f987aa110a529c32b0797fded11141dae5
EK <ekim0480@gmail.com>
2021-06-18 16:03:54 -0500

    added pagination to tables

--- public/js/customer-details.js
+++ public/js/customer-details.js
@@ -134,6 +134,31 @@ $(document).ready(function () {
       salesToAdd.push(createSaleRow(sales[i]));
     }
     renderSaleList(salesToAdd);
+
+    $("#saleTable").after('<div id="nav"></div>');
+    var rowsShown = 10;
+    var rowsTotal = $("#saleTable tbody tr").length;
+    var numPages = rowsTotal / rowsShown;
+    for (i = 0; i < numPages; i++) {
+      var pageNum = i + 1;
+      $("#nav").append('<a href="#" rel="' + i + '">' + pageNum + "</a> ");
+    }
+    $("#saleTable tbody tr").hide();
+    $("#saleTable tbody tr").slice(0, rowsShown).show();
+    $("#nav a:first").addClass("active");
+    $("#nav a").bind("click", function () {
+      $("#nav a").removeClass("active");
+      $(this).addClass("active");
+      var currPage = $(this).attr("rel");
+      var startItem = currPage * rowsShown;
+      var endItem = startItem + rowsShown;
+      $("#saleTable tbody tr")
+        .css("opacity", "0.0")
+        .hide()
+        .slice(startItem, endItem)
+        .css("display", "table-row")
+        .animate({ opacity: 1 }, 300);
+    });
   }
 
   // function to render sales rows, if present
--- public/js/customer-list.js
+++ public/js/customer-list.js
@@ -34,11 +34,37 @@ $(document).ready(function () {
   function getCustomers() {
     $.get("/customers", function (data) {
       var rowsToAdd = [];
+      console.log(data);
       for (var i = 0; i < data.length; i++) {
         rowsToAdd.push(createCustomerRow(data[i]));
       }
       renderCustomerList(rowsToAdd);
-      //   nameInput.val("");
+
+      // table pagination
+      $("#customerTable").after('<div id="nav"></div>');
+      var rowsShown = 10;
+      var rowsTotal = $("#customerTable tbody tr").length;
+      var numPages = rowsTotal / rowsShown;
+      for (i = 0; i < numPages; i++) {
+        var pageNum = i + 1;
+        $("#nav").append('<a href="#" rel="' + i + '">' + pageNum + "</a> ");
+      }
+      $("#customerTable tbody tr").hide();
+      $("#customerTable tbody tr").slice(0, rowsShown).show();
+      $("#nav a:first").addClass("active");
+      $("#nav a").bind("click", function () {
+        $("#nav a").removeClass("active");
+        $(this).addClass("active");
+        var currPage = $(this).attr("rel");
+        var startItem = currPage * rowsShown;
+        var endItem = startItem + rowsShown;
+        $("#customerTable tbody tr")
+          .css("opacity", "0.0")
+          .hide()
+          .slice(startItem, endItem)
+          .css("display", "table-row")
+          .animate({ opacity: 1 }, 300);
+      });
     });
   }
 
@@ -78,6 +104,7 @@ $(document).ready(function () {
         $("#customerTable tbody").empty();
         rowsToAdd.push(createCustomerRow(data));
         renderCustomerList(rowsToAdd);
+        $("#nav").remove();
       }
     });
   });
