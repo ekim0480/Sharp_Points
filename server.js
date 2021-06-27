4a96639d3658d25db8ae396a70145b47a1527a15
EK <ekim0480@gmail.com>
2021-06-26 22:04:23 -0500

    bugfix attempt

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 // For Passport
 app.use(
   session({
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.secret,
     resave: false,
     saveUninitialized: false,
   })
@@ -49,7 +49,7 @@ require("./routes/html-routes.js")(app, passport);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: false }).then(function () {
+db.sequelize.sync({ force: true }).then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
