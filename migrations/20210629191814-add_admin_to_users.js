58417cdfcf9a8fce0b0d564918129e3bca37d911
EK <ekim0480@gmail.com>
2021-06-29 14:20:40 -0500

    pre migration, added hasAdmin column to users table

---
+++ migrations/20210629191814-add_admin_to_users.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const { query } = require("express");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     return queryInterface.addColumn('Users', 'hasAdmin', {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+      after: "password"
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.removeColumn('Users', 'hasAdmin')
+  }
+};
